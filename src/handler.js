a739e928bbcca95dc8cdd06d0b33f93b0b543b30
Fatima Azzahro <azzahro.fatima@gmail.com>
2021-05-30 17:06:44 +0700

    Save Note - Generate time of create and update Note

--- src/handler.js
+++ src/handler.js
@@ -3,4 +3,6 @@ const { nanoid } = require('nanoid');
 const addNoteHandler = (request, h) => {
   const { title, tags, body } = request.payload;
   const id = nanoid(16);
+  const createdAt = new Date().toISOString();
+  const updatedAt = createdAt;
 };
