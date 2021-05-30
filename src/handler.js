b600310f9a6546345fe2a1b9cac3c55f0512384f
Fatima Azzahro <azzahro.fatima@gmail.com>
2021-05-30 17:49:01 +0700

    Show Note Detail - Check whether Note Detail is found or not

--- src/handler.js
+++ src/handler.js
@@ -47,6 +47,22 @@ const getNoteByIdHandler = (request, h) => {
   const { id } = request.params;
 
   const note = notes.filter((n) => n.id === id)[0];
+
+  if (note !== undefined) {
+    return {
+      status: 'success',
+      data: {
+        note,
+      },
+    };
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Note cannot be found!',
+  });
+  response.code(404);
+  return response;
 };
 
 module.exports = { addNoteHandler, getAllNotesHandler };
