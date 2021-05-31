0e90af2040eb48a4468355f1533f59e854e079a1
Fatima Azzahro <azzahro.fatima@gmail.com>
2021-05-31 08:18:33 +0700

    Edit Note Detail - Update saved Note object by Id

--- src/handler.js
+++ src/handler.js
@@ -69,6 +69,17 @@ const editNoteByIdHandler = (request, h) => {
   const { id } = request.params;
   const { title, tags, body } = request.payload;
   const updatedAt = new Date().toISOString();
+
+  const index = notes.findIndex((note) => note.id === id);
+
+  if (index !== -1) {
+    notes[index] = {
+      ...notes[index],
+      title,
+      tags,
+      body,
+      updatedAt,
+    };
 };
 
 module.exports = { addNoteHandler, getAllNotesHandler, getNoteByIdHandler };
